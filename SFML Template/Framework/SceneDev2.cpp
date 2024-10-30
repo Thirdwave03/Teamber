e03e4c451bb351e7d2ed0c88ba159a81201e40da
문세진 <lunatic1209@naver.com>
2024-10-30 15:41:34 +0900

    나무셋팅 플레이어 추가세팅중

--- SFML Template/Framework/SceneDev2.cpp
+++ SFML Template/Framework/SceneDev2.cpp
@@ -33,6 +33,7 @@ void SceneDev2::Init()
 	TEXTURE_MGR.Load("graphics/tree.png");
 	TEXTURE_MGR.Load("graphics/branch.png");
 	TEXTURE_MGR.Load("graphics/player.png");
+	TEXTURE_MGR.Load("graphics/player2.png");
 	TEXTURE_MGR.Load("graphics/rip.png");
 	TEXTURE_MGR.Load("graphics/axe.png");
 
@@ -46,7 +47,34 @@ void SceneDev2::Init()
 	tree2->sortingLayer = SortingLayers::Background;
 	tree2->sortingOrder = -1;
 	tree2->SetOrigin(Origins::MC);
-	tree2->SetPosition({ (1920 / 4) * 3, 850 });
+	tree2->SetPosition({ (1920 / 4) + 960 , 850 });
+
+	player1 = AddGo(new Player("Player"));
+	player2 = AddGo(new Player("graphics/player2.png"));
+
+
+	centerMsg = AddGo(new TextGo("fonts/KOMIKAP_.ttf", "Center Message"));
+	centerMsg->sortingLayer = SortingLayers::UI;
+
+	uiScore = AddGo(new UiScore("fonts/KOMIKAP_.ttf", "Ui Score"));
+	uiTimer = AddGo(new UiTimebar("Ui Timer"));
+
+	Scene::Init();
+
+	player1->SetPosition({ 1920 / 4, 850 });
+	player2->SetPosition({ (1920 / 4) + 960 , 850 });
+
+	centerMsg->text.setCharacterSize(100);
+	centerMsg->text.setFillColor(sf::Color::White);
+	centerMsg->SetPosition({ 1920.f / 2.f, 1080.f / 2.f });
+
+	uiScore->text.setCharacterSize(75);
+	uiScore->text.setFillColor(sf::Color::White);
+	uiScore->SetPosition({ 30.f, 30.f });
+
+	uiTimer->Set({ 500.f, 100.f }, sf::Color::Red);
+	uiTimer->SetOrigin(Origins::ML);
+	uiTimer->SetPosition({ 1920.f / 2.f - 250.f, 1080.f - 100.f });
 
 	Scene::Init();
 }
@@ -54,34 +82,197 @@ void SceneDev2::Init()
 void SceneDev2::Enter()
 {
 
+	TEXTURE_MGR.Load("graphics/background.png");
+	TEXTURE_MGR.Load("graphics/cloud.png");
 	TEXTURE_MGR.Load("graphics/tree.png");
-
-
+	TEXTURE_MGR.Load("graphics/branch.png");
+	TEXTURE_MGR.Load("graphics/log.png");
 	TEXTURE_MGR.Load("graphics/player.png");
+	TEXTURE_MGR.Load("graphics/player2.png");
+	TEXTURE_MGR.Load("graphics/rip.png");
+	TEXTURE_MGR.Load("graphics/axe.png");
+	FONT_MGR.Load("fonts/KOMIKAP_.ttf");
+	SOUNDBUFFER_MGR.Load("sound/chop.wav");
+	SOUNDBUFFER_MGR.Load(sbIdDeath);
+	SOUNDBUFFER_MGR.Load(sbIdTimeOut);
+
+	sfxDeath.setBuffer(SOUNDBUFFER_MGR.Get(sbIdDeath));
+	sfxTimeOut.setBuffer(SOUNDBUFFER_MGR.Get(sbIdTimeOut));
 
 	Scene::Enter();
+
+	SetStatus(Status::Awake);
 }
 
 void SceneDev2::Exit()
 {
-	std::cout << "SceneDev2::Exit()" << std::endl;
+	std::cout << "SceneDev1::Exit()" << std::endl;
+
+	player1->SetSceneGame(nullptr);
+	tree->ClearEffectLog();
 
 	Scene::Exit();
 
+	TEXTURE_MGR.Unload("graphics/cloud.png");
+	TEXTURE_MGR.Unload("graphics/background.png");
+	TEXTURE_MGR.Unload("graphics/tree.png");
+	TEXTURE_MGR.Unload("graphics/branch.png");
+	TEXTURE_MGR.Unload("graphics/log.png");
 	TEXTURE_MGR.Unload("graphics/player.png");
+	TEXTURE_MGR.Unload("graphics/player2.png");
+	TEXTURE_MGR.Unload("graphics/rip.png");
+	TEXTURE_MGR.Unload("graphics/axe.png");
+	FONT_MGR.Unload("fonts/KOMIKAP_.ttf");
+	SOUNDBUFFER_MGR.Unload("sound/chop.wav");
+	SOUNDBUFFER_MGR.Unload("sound/death.wav");
+	SOUNDBUFFER_MGR.Unload("sound/out_of_time.wav");
 }
 
 void SceneDev2::Update(float dt)
 {
 	Scene::Update(dt);
 
-	if (InputMgr::GetKeyDown(sf::Keyboard::Space))
+	switch (currentStatus)
 	{
-		SCENE_MGR.ChangeScene(SceneIds::Dev1);
+	case SceneDev2::Status::Awake:
+		UpdateAwake(dt);
+		break;
+	case SceneDev2::Status::Game:
+		UpdateGame(dt);
+		break;
+	case SceneDev2::Status::GameOver:
+		UpdateGameOver(dt);
+		break;
+	case SceneDev2::Status::Pause:
+		UpdatePause(dt);
+		break;
 	}
 }
 
 void SceneDev2::Draw(sf::RenderWindow& window)
 {
 	Scene::Draw(window);
+}
+
+void SceneDev2::SetCenterMessage(const std::string& msg)
+{
+	centerMsg->text.setString(msg);
+	centerMsg->SetOrigin(Origins::MC);
+}
+
+void SceneDev2::SetVisibleCenterMessage(bool visible)
+{
+	centerMsg->SetActive(visible);
+}
+
+void SceneDev2::SetScore(int score)
+{
+	this->score = score;
+	uiScore->SetScore(this->score);
+}
+
+void SceneDev2::SetStatus(Status newStatus)
+{
+	Status prevStatus = currentStatus;
+	currentStatus = newStatus;
+
+	switch (currentStatus)
+	{
+	case SceneDev2::Status::Awake:
+		FRAMEWORK.SetTimeScale(0.f);
+		SetVisibleCenterMessage(true);
+		SetCenterMessage("Press Enter To Start!!");
+		score = 0;
+		timer = gameTime;
+		SetScore(score);
+		uiTimer->SetValue(1.f);
+		break;
+	case SceneDev2::Status::Game:
+		if (prevStatus == Status::GameOver)
+		{
+			score = 0;
+			timer = gameTime;
+
+			SetScore(score);
+			uiTimer->SetValue(1.f);
+
+			player1->Reset();
+			tree->Reset();
+		}
+		FRAMEWORK.SetTimeScale(1.f);
+		SetVisibleCenterMessage(false);
+		break;
+	case SceneDev2::Status::GameOver:
+		FRAMEWORK.SetTimeScale(0.f);
+		SetVisibleCenterMessage(true);
+		break;
+	case SceneDev2::Status::Pause:
+		FRAMEWORK.SetTimeScale(0.f);
+		SetVisibleCenterMessage(true);
+		SetCenterMessage("PAUSE! ESC TO RESUME!");
+		break;
+	}
+}
+
+void SceneDev2::UpdateAwake(float dt)
+{
+	if (InputMgr::GetKeyDown(sf::Keyboard::Enter))
+	{
+		SetStatus(Status::Game);
+	}
+}
+
+void SceneDev2::UpdateGame(float dt)
+{
+	if (InputMgr::GetKeyDown(sf::Keyboard::Escape))
+	{
+		SetStatus(Status::Pause);
+		return;
+	}
+
+	timer = Utils::Clamp(timer - dt, 0.f, gameTime);
+	uiTimer->SetValue(timer / gameTime);
+	if (timer <= 0.f)
+	{
+		sfxTimeOut.play();
+
+		player1->OnDie();
+		SetCenterMessage("Time Over!");
+		SetStatus(Status::GameOver);
+		return;
+	}
+}
+
+void SceneDev2::UpdateGameOver(float dt)
+{
+	if (InputMgr::GetKeyDown(sf::Keyboard::Enter))
+	{
+		SetStatus(Status::Game);
+	}
+}
+
+void SceneDev2::UpdatePause(float dt)
+{
+	if (InputMgr::GetKeyDown(sf::Keyboard::Escape))
+	{
+		SetStatus(Status::Game);
+	}
+}
+
+void SceneDev2::OnChop(Sides side)
+{
+	Sides branchSide = tree->Chop(side);
+	if (player1->GetSide() == branchSide)
+	{
+		sfxDeath.play();
+
+		player1->OnDie();
+		SetCenterMessage("You Die!");
+		SetStatus(Status::GameOver);
+	}
+	else
+	{
+		SetScore(score + 100);
+		timer += 1.f;
+	}
 }
